359fc98dd47bbaf234eb88360cc0edfd3d0038df
izhak moalem <izhakmo@post.bgu.ac.il>
2019-12-17 00:17:00 +0200

    started working on the broker hypothetically

--- src/main/java/bgu/spl/mics/MessageBrokerImpl.java
+++ src/main/java/bgu/spl/mics/MessageBrokerImpl.java
@@ -1,4 +1,5 @@
 package bgu.spl.mics;
+import java.util.concurrent.*;
 
 /**
  * The {@link MessageBrokerImpl class is the implementation of the MessageBroker interface.
@@ -7,13 +8,24 @@ package bgu.spl.mics;
  */
 public class MessageBrokerImpl implements MessageBroker {
 
+	private ConcurrentHashMap<Subscriber,LinkedBlockingQueue<Message>> mapOfSubscribers;	//not sure about the key and the value, we need a Queue
+//	private ConcurrentHashMap<>mapOfTopics;	TODO
+	private ConcurrentHashMap<Event,Future> mapOfEvents;
+
+	private static class MessageBrokerHolder{
+		private static MessageBrokerImpl instance= new MessageBrokerImpl();
+	}
+
 	/**
 	 * Retrieves the single instance of this class.
 	 */
 	public static MessageBroker getInstance() {
 		//TODO: Implement this
-		return null;
+		return MessageBrokerHolder.instance;
 	}
+
+
+
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber m) {
@@ -30,6 +42,7 @@ public class MessageBrokerImpl implements MessageBroker {
 	@Override
 	public <T> void complete(Event<T> e, T result) {
 		// TODO Auto-generated method stub
+		mapOfEvents.get(e).resolve(result);
 
 	}
 
@@ -47,20 +60,22 @@ public class MessageBrokerImpl implements MessageBroker {
 	}
 
 	@Override
-	public void register(Subscriber m) {
+	public void register(Subscriber m) {	//assume we adding the sub name as key,and sub to the map
 		// TODO Auto-generated method stub
+		mapOfSubscribers.put(m,new LinkedBlockingQueue<Message>());
 
 	}
 
 	@Override
-	public void unregister(Subscriber m) {
+	public void unregister(Subscriber m) {	//similar to the register, but we need to clear his EVENT queue TODO
 		// TODO Auto-generated method stub
-
+		mapOfSubscribers.remove(m);
 	}
 
 	@Override
 	public Message awaitMessage(Subscriber m) throws InterruptedException {
 		// TODO Auto-generated method stub
+
 		return null;
 	}
 
