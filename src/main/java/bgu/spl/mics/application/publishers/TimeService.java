00dcc87a5f8cdb7b51d6e4cb6559073806796725
ziv232 <zivshir@post.bgu.ac.il>
2019-12-17 20:40:38 +0200

    TimeService update

--- src/main/java/bgu/spl/mics/application/publishers/TimeService.java
+++ src/main/java/bgu/spl/mics/application/publishers/TimeService.java
@@ -1,6 +1,10 @@
 package bgu.spl.mics.application.publishers;
-
+import bgu.spl.mics.Broadcast;
+import bgu.spl.mics.MessageBrokerImpl;
 import bgu.spl.mics.Publisher;
+import bgu.spl.mics.application.subscribers.TickBroadcast;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * TimeService is the global system timer There is only one instance of this Publisher.
@@ -12,21 +16,42 @@ import bgu.spl.mics.Publisher;
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class TimeService extends Publisher {
+	private int step;
+	private int duration;
+	private AtomicInteger tick;
+	//private long startTime;
 
-	public TimeService() {
-		super("Change_This_Name");
-		// TODO Implement this
+
+
+	public TimeService(int duration) {
+		super("clock");
+		this.step = 100;
+		this.duration = duration;
+		this.tick.set(1);
+		//this.startTime = 0;
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
+		Thread thread = new Thread(this);
 		
 	}
 
 	@Override
 	public void run() {
-		// TODO Implement this
+		//this.startTime=System.currentTimeMillis();
+		while(tick.get()<duration){
+			try { Thread.sleep(step); }
+			catch (InterruptedException e){ throw new IllegalStateException(e.getMessage()); }
+			tick.incrementAndGet();
+
+			MessageBrokerImpl.getInstance().sendBroadcast((Broadcast) new TickBroadcast(tick.get()));
+
+			//int currentTick = (int)((System.currentTimeMillis()-startTime)/step);
+			//tick.compareAndSet(tick,currentTick);
+
+		}
+
 	}
 
 }
